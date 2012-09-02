b650256e0e473f0dba943426efbe51c891cad175
mislabeled <mislabeled@email>
2012-09-02 12:49:15 -0500

    cleaned up

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ from BeautifulSoup import BeautifulSoup
 SUBREDDIT = 'PoliticalFactChecks'
 USERNAME = 'PoliticalFactChecks'
 USER_AGENT = 'PoliticalFactChecksBot/0'
-TRUTH_O_METER_RSS = 'http://www.politifact.com/feeds/statements/truth-o-meter/'
-FACT_CHECK_ORG_RSS = 'http://factcheck.org/feed/rss/'
-WAPO_FACT_CHECKER = 'http://feeds.washingtonpost.com/rss/rss_fact-checker'
+POLITIFACT_RSS = 'http://www.politifact.com/feeds/statements/truth-o-meter/'
+FACTCHECK_ORG_RSS = 'http://factcheck.org/feed/rss/'
+WAPO_FACT_CHECKER_RSS = 'http://feeds.washingtonpost.com/rss/rss_fact-checker'
 
 SOURCE_POLITIFACT = 'Politifact'
 SOURCE_FACTCHECK_ORG = 'FactCheck.org'
@@ -61,40 +61,48 @@ class Submission:
         return ''.join(BeautifulSoup(self.description).findAll(text=True)).replace('... >> More', '').replace('&#8230; More >>', '').replace('Read full article  &gt;&gt;', '').replace('&amp;', '&')
     
     def get_verdict(self):
-        if self.source_name == SOURCE_POLITIFACT:
-            return self._getPolitifactVerdict()
-        elif self.source_name == SOURCE_FACTCHECK_ORG:
-            return self._getFactcheckOrgVerdict()
-        elif self.source_name == SOURCE_WAPO:
-            return self._getWashingtonPostVerdict()
         return None
+
+class PolitifactSubmission(Submission):
+    def __init__(self, url, guid, title, link, description):
+        Submission.__init__(self, SOURCE_POLITIFACT, url, guid, title, link, description)
     
-    def _getPolitifactVerdict(self):
+    def get_verdict(self):
         """ read first few words from description: 'The Truth-o-Meter says: Half-True' """
         if self.description.startswith('The Truth-o-Meter says:'):
             desc_split = self.description.split('|')
             if len(desc_split) > 0:
                 return desc_split[0].replace('The Truth-o-Meter says:', '').strip()
         return None
+
+class FactcheckOrgSubmission(Submission):
+    def __init__(self, url, guid, title, link, description):
+        Submission.__init__(self, SOURCE_FACTCHECK_ORG, url, guid, title, link, description)
     
-    def _getFactcheckOrgVerdict(self):
+    def get_verdict(self):
         # TODO: looking for some consistent scheme
         return None
+
+class WashingtonPostSubmission(Submission):
+    def __init__(self, url, guid, title, link, description):
+        Submission.__init__(self, SOURCE_WAPO, url, guid, title, link, description)
     
-    def _getWashingtonPostVerdict(self):
+    def get_verdict(self):
         """ look for images on full page: pinocchio_1.jpg, pinocchio_2.jpg, pinocchio_3.jpg, or pinocchio_4.jpg """
         soup = BeautifulSoup(urllib2.urlopen(self.link))
         image_src_list = [image['src'] for image in soup.findAll('img', attrs={'src' : re.compile('pinocchio_(\d)\.jpg')})]
-        verdicts = [self._getPinocchioText(re.search('pinocchio_(\d)\.jpg', img_src).group(1)) for img_src in image_src_list]
+        verdicts = [self._get_pinocchio_text(re.search('pinocchio_(\d)\.jpg', img_src).group(1)) for img_src in image_src_list]
         if len(verdicts) > 0:
             return ', '.join(verdicts)
         return None
     
-    def _getPinocchioText(self, nose_count):
+    def _get_pinocchio_text(self, nose_count):
         if nose_count == '1':
             return '%s Pinocchio' % nose_count
         return '%s Pinocchios' % nose_count
-    
+        
+
+#### database methods ####
 def already_been_posted(url, guid):
     c.execute('select id from submission where url = ? and guid = ? limit 1', (url, guid))
     result = c.fetchone()
@@ -105,45 +113,42 @@ def already_been_posted(url, guid):
 def set_posted(url, guid):
     c.execute("insert into submission(url, guid) values ('%s', '%s')" % (url, guid))
     conn.commit()
+   
 
-def get_submissions(source_name, url):
-    submissions = []
-    truth_o_meter_feed = feedparser.parse(url)
-    for entry in truth_o_meter_feed.entries:
-        guid = entry['guid']
-        title = entry['title']
-        link = entry['link']
-        description = entry['description']
-        if not already_been_posted(url, guid):
-            submissions.append(Submission(source_name, url, guid, title, link, description))
-    return submissions
-            
+#### feed/submission methods ####
+def get_feed_entries(url):
+    return feedparser.parse(url).entries
+
+def get_submissions(clazz, url):
+    return [clazz(url, entry['guid'], entry['title'], entry['link'], entry['description']) for entry in get_feed_entries(url) if not already_been_posted(url, entry['guid'])]
 
 def get_politifact_submissions():
-    return get_submissions(SOURCE_POLITIFACT, TRUTH_O_METER_RSS)
+    return get_submissions(PolitifactSubmission, POLITIFACT_RSS)
 
 def get_factcheckorg_submissions():
-    return get_submissions(SOURCE_FACTCHECK_ORG, FACT_CHECK_ORG_RSS)
+    return get_submissions(FactcheckOrgSubmission, FACTCHECK_ORG_RSS)
 
 def get_wapofactchecker_submissions():
-    return get_submissions(SOURCE_WAPO, WAPO_FACT_CHECKER)
+    return get_submissions(WashingtonPostSubmission, WAPO_FACT_CHECKER_RSS)
 
 
 def read_password_from_file():
     return open('password.txt').read()
     
-def main():
-    r = reddit.Reddit(user_agent=USER_AGENT)
-    r.login(USERNAME, read_password_from_file())
-    
+def main():    
     submissions = []
     submissions.extend(get_politifact_submissions())
     submissions.extend(get_factcheckorg_submissions())
     submissions.extend(get_wapofactchecker_submissions())
     
-    for submission in submissions:
-        r.submit(SUBREDDIT, submission.get_title(), submission.get_text())
-        set_posted(submission.get_url(), submission.get_guid())
+    if len(submissions) > 0:
+        r = reddit.Reddit(user_agent=USER_AGENT)
+        r.login(USERNAME, read_password_from_file())
+        
+        for submission in submissions:
+            print "submitting %s" % submission.get_guid()
+            r.submit(SUBREDDIT, submission.get_title(), submission.get_text())
+            set_posted(submission.get_url(), submission.get_guid())
 
     return 0
 
